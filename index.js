dad8b0d971c5ec361cf791a8340b680050f4d2a4
GongXian Cao <gongxiancao@hotmail.com>
2016-09-29 00:07:48 +0800

    add first implementation

---
+++ index.js
@@ -0,0 +1,99 @@
+
+var _ = require('lodash'),
+  fs = require('fs'),
+  Sequelize = require('sequelize'),
+  pathUtil = require('path'),
+  Promise = require('bluebird');
+
+function lift (done) {
+  var self = this;
+  var modelsConfig = self.config.models;
+  var defaultConnectionName = modelsConfig.connection;
+
+  // expose Sequelize
+  global.Sequelize = Sequelize;
+
+  var modelsPath = self.config.paths.models = pathUtil.join(self.config.paths.root, 'api/models');
+
+  var readdirAsync = Promise.promisify(fs.readdir),
+    statAsync = Promise.promisify(fs.stat);
+
+  readdirAsync(modelsPath)
+    .then(function (fileNames) {
+      var filePaths = _.map(fileNames, function (fileName) {
+        return pathUtil.join(modelsPath, fileName);
+      });
+
+      return [fileNames, filePaths, Promise.map(filePaths, function (filePath) {
+        var extname = pathUtil.extname(filePath);
+        if(extname !== '.js') {
+          return null;
+        }
+        return statAsync(filePath);
+      })];
+    })
+    .spread(function (fileNames, filePaths, fileStats) {
+      var connections = {};
+      var models = {};
+      // get model definitions and connection definitions
+      _.each(fileNames, function (fileName, index) {
+        var stat = fileStats[index];
+        if(!stat || !stat.isFile()) {
+          return;
+        }
+
+        var filePath = filePaths[index];
+        var model = require(filePath);
+        var modelName = pathUtil.basename(fileName, '.js');
+
+        models[modelName] = model;
+        model.options = model.options || {};
+
+        // cache connection config
+        var connectionName = model.options.connection = model.options.connection || defaultConnectionName;
+        var connectionConfig = self.config.connections[connectionName];
+        if(!connectionConfig) {
+          throw new Error('cannot find connection config for ' + connectionName);
+        }
+        connections[connectionName] = connectionConfig;
+      });
+
+      // create used connections
+      connections = _.mapValues(connections, function (connectionConfig) {
+        var connection = new Sequelize(
+          connectionConfig.database,
+          connectionConfig.username,
+          connectionConfig.password,
+          _.extend({}, modelsConfig.options, {host: connectionConfig.host}));
+        if(modelsConfig.sync) {
+          connection.sync();
+        }
+        return connection;
+      });
+
+      self.models = _.mapValues(models, function (model, modelName) {
+        var connectionName = model.options.connection || defaultConnectionName;
+        return connections[connectionName].define(modelName, model.attributes);
+      });
+      _.extend(global, self.models);
+      return models;
+    })
+    .then(function (models) {
+      _.each(models, function (model, modelName) {
+        if(model.associationInitializer) {
+          model.associationInitializer();          
+        }
+      });
+    })
+    .then(_.ary(done, 0))
+    .catch(done);
+}
+
+function lower (done) {
+  process.nextTick(done);
+}
+
+module.exports = {
+  lift: Promise.promisify(lift),
+  lower: Promise.promisify(lower)
+};
